0d1c7f1ee735eeb3e21a154f90c573afa05a8ee5
Nathalya <Nathalyaxavier99@gmail.com>
2025-09-08 21:52:04 -0300

    feat: novas atualizaçao.

--- backend/MeuCorre.Domain/Entities/Usuario.cs
+++ backend/MeuCorre.Domain/Entities/Usuario.cs
@@ -8,12 +8,24 @@
         public DateTime DataNascimento { get; private set; }
         public bool Ativo { get; private set; }
 
+        // Propriedade de navegação para a entidade Categoria pois
+        // o usuário pode ter várias categorias
+        public virtual ICollection<Categoria> Categorias { get; set; }
+
+
         //Construtor para criar um novo usuário.
         //Construtor é a primeira coisa que é executada quando uma classe é instanciada.
-        public Usuario(string nome, string email, DateTime dataNascimento, bool ativo)
+        public Usuario(string nome, string email, string senha, DateTime dataNascimento, bool ativo)
         {
+
+            if (!TemIdadeMinima())
+            {
+                throw new Exception("Usuário deve ter no mínimo 13 anos.");
+            }
+
             Nome = nome;
             Email = email;
+            Senha = ValidarSenha(senha);
             DataNascimento = dataNascimento;
             Ativo = ativo;
         }
@@ -34,6 +46,15 @@
         {
             var resultado = CalcularIdade() >= 13;
             return resultado;
+        }
+
+        public string ValidarSenha(string senha)
+        {
+            if (senha.Length < 6)
+            {
+                //Todo Fazer um tratamento de erro melhor
+            }
+            return senha;
         }
 
         public void AtivarUsuario()
