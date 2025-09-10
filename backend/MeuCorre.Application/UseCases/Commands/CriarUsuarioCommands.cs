75808fde948ed1c47d5b89ac80c6282b1b991554
Nathalya <Nathalyaxavier99@gmail.com>
2025-09-09 21:30:26 -0300

    feat:Adicionando o controlle.

---
+++ backend/MeuCorre.Application/UseCases/Commands/CriarUsuarioCommands.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuCorre.Application.UseCases.Commands
+{
+    /// <summary>
+    /// <Comando para criar um novo usuário>
+    /// aqui você pode adicionar propriedades necessárias para criar o usuário, como Nome, Email, Senha, etc.
+    /// </summary>
+    public class CriarUsuarioCommands : IRequest<string>
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public DateTime DataNascimento { get; set; } 
+    }
+}
