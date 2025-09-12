153d1208f3d20f0daf7592cf5b722ea89d3abd55
Nathalya <Nathalyaxavier99@gmail.com>
2025-09-11 21:54:29 -0300

    feat: Atualizaçao dos codigos.

---
+++ backend/MeuCorre.Application/UseCases/Usuarios/Commands/AtualizarUsuarioCommand.cs
@@ -0,0 +1,52 @@
+﻿using MediatR;
+using MeuCorre.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuCorre.Application.UseCases.Usuarios.Commands
+{
+    public class AtualizarUsuarioCommand : IRequest
+    {
+
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        public required string Nome { get; set; }
+
+        [Required(ErrorMessage = "Email é obrigatório")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Data de Nascimento é obrigatória")]
+        public DateTime DataNascimento { get; set; }
+    }
+
+    internal class AtualizarUsuarioCommandHandler : IRequestHandler<AtualizarUsuarioCommand>
+    {
+        public Task<Unit> Handle(AtualizarUsuarioCommand request, CancellationToken cancellationToken)
+        {
+            // Implementar a lógica de atualização do usuário aqui
+            throw new NotImplementedException();
+        }
+    }
+    
+      public async Task<(string, bool)>Handle(AtualizarUsuarioCommand request, CancellationToken cancellationToken)
+        {
+            var usuario = await UsuarioRepository.ObterUsuarioPorEmail(request.Email);
+            if (usuario == null)
+            {
+                return ("Usuário não encontrado.", false);
+            }
+
+            // Atualiza apenas os campos permitidos
+            usuario.AtualizarDados(
+                nome: request.Nome,
+                dataNascimento: request.DataNascimento
+            );
+
+            await UsuarioRepository.AtualizarUsuarioAsync(usuario);
+            return ("Usuário atualizado com sucesso.", true);
+        }
+
+    }
--- backend/MeuCorre.Domain/Entities/Usuario.cs
+++ backend/MeuCorre.Domain/Entities/Usuario.cs
@@ -1,4 +1,6 @@
-﻿namespace MeuCorre.Domain.Entities
+﻿using System.Text.RegularExpressions;
+
+namespace MeuCorre.Domain.Entities
 {
     public class Usuario : Entidade
     {
@@ -8,32 +10,59 @@
         public DateTime DataNascimento { get; private set; }
         public bool Ativo { get; private set; }
 
+        // Propriedade de navegação para a entidade Categoria pois
+        // o usuário pode ter várias categorias
+        public virtual ICollection<Categoria> Categorias { get; set; }
+
+
         //Construtor para criar um novo usuário.
         //Construtor é a primeira coisa que é executada quando uma classe é instanciada.
-        public Usuario(string nome, string email, DateTime dataNascimento, bool ativo)
+        public Usuario(string nome, string email, string senha, DateTime dataNascimento, bool ativo)
         {
             Nome = nome;
             Email = email;
-            DataNascimento = dataNascimento;
+            Senha = ValidarSenha(senha);
+            DataNascimento = ValidarIdadeMinima(dataNascimento);
             Ativo = ativo;
         }
 
         //Regra negocio: Permite apenas usários maiores de 13 anos.
-        private int CalcularIdade()
+        private DateTime ValidarIdadeMinima(DateTime nascimento)
         {
             var hoje = DateTime.Today;
-            var idade = hoje.Year - DataNascimento.Year;
+            var idade = hoje.Year - nascimento.Year;
 
-            if (DataNascimento.Date > hoje.AddYears(-idade))
+            if (nascimento.Date > hoje.AddYears(-idade))
                 idade--;
 
-            return idade;
-        }
+            if (idade < 13)
+            {
+                //interrompe o processo devolvendo o erro.
+                throw new Exception("Usuário deve ter no minimo 13 anos.");
+            }
 
-        private bool TemIdadeMinima()
+            return nascimento;
+
+        }
+       
+        public string ValidarSenha(string senha)
         {
-            var resultado = CalcularIdade() >= 13;
-            return resultado;
+            //Regra de negocio: pelo menos uma letra e um número.
+            
+            if (!Regex.IsMatch(senha, "[a-z]"))
+            {
+                throw new Exception("A senha deve contar pelo menos uma letra minuscula");
+            }
+            if (!Regex.IsMatch(senha, "[A-Z]"))
+            {
+                throw new Exception("A senha deve contar pelo menos uma letra maiuscula");
+            }
+            if (!Regex.IsMatch(senha, "[0-9]"))
+            {
+                throw new Exception("A senha deve contar pelo menos um números");
+            }
+
+            return senha;
         }
 
         public void AtivarUsuario()
